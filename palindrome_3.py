811e0d4432e34dfba6d5f9e8bd24fffcdccae009
luchian <luchian-1999@mail.ru>
2025-01-07 01:51:06 +0300

    Palindrome_3.py: came up with solution using sliding windows

--- palindrome_3.py
+++ palindrome_3.py
@@ -12,16 +12,16 @@ class Solution:
 
 
     def longestPalindrome(self, s: str) -> str:
-        #returns the longest palindrom
+        #returns the longest palindrome
+        #here I will use sliding window
         if len(s) == 1:
-            return s
-        all_palinds = []
-        for ind in range(len(s)-1):
-        	for sub_ind in range(-1,ind-len(s)-1,-1): # this backward trick is useful 
-        		if s[ind] == s[sub_ind]:
-        			check_string = s[ind:] if sub_ind == -1 else s[ind:sub_ind+1]
-        			if self.is_palin(check_string): # the problem with adding to negative index
-        				all_palinds.append(check_string)
-        				break
-        return max(all_palinds,key = lambda x: len(x))
- 
+        	return s
+        all_palins = [s[0]]
+        for N in range(2,len(s)+1):
+        	for i in range(len(s)-(N-1)):
+        		if self.is_palin(s[i:i+N]):
+        			all_palins.append(s[i:i+N])
+        			break
+        return max(all_palins,key = lambda x: len(x))
+
+
